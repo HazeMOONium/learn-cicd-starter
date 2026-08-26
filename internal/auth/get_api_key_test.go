e1998386779c122b7f7e08cc2cf4b4c36fd41fff
HazeMOONium <hazemnasr@aucegypt.edu>
2026-08-27 02:51:09 +0300

    add unit tests for GetApiKey

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,36 @@
+package auth
+
+import (
+	"errors"
+	"testing"
+	"net/http"
+)
+
+func httpHeader(key, val string) http.Header {
+	return http.Header{key: []string{val}}
+}
+
+
+
+func TestGetAPIKey(t *testing.T) {
+	tests := map[string]struct{
+		input	 	http.Header
+		wantString	string
+		wantError	error
+	}{
+		"normal": {input: httpHeader("Authorization", "ApiKey validAPIKey"), wantString: "validAPIKey", wantError: nil},
+		"missing auth header": {input: http.Header{}, wantString: "", wantError: ErrNoAuthHeaderIncluded},
+		"missing api key": {input: httpHeader("Authorization", "ApiKey"), wantString: "", wantError: ErrMalformedAuthHeader},
+		"missing api key with trailing space": {input: httpHeader("Authorization", "ApiKey "), wantString: "", wantError: errors.New("malformed authorization header")},
+		"malformed auth header val": {input: httpHeader("Authorization", "APIKey validAPIKey"), wantString: "", wantError: ErrMalformedAuthHeader},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T){
+			gotString, gotError := GetAPIKey(tc.input)
+			if gotString != tc.wantString || !errors.Is(gotError, tc.wantError) {
+				t.Fatalf("wantString: %v, gotString: %v, wantError: %v, gotError: %v", tc.wantString, gotString, tc.wantError, gotError)
+			}
+		})
+	}
+}
